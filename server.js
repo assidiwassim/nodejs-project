12521bdeaffb010c1d65eaa26c6d3e6b3086c417
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-07-04 21:24:51 +0100

    add users endpoit

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/users', (req, res) => {
+  res.send('users')
+})
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
