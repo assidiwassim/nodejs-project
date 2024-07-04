26708a4959b38f32f36ad9ddf006dc067533f5c8
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-07-04 21:07:04 +0100

    remove post endpoint

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/add-user', (req, res) => {
-  res.send('user added ....')
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
